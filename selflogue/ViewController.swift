767340a407147d13ab3cd74830060caf4ef858a5
Chew Jun PIn <jche0371@student.monash.edu>
2023-04-26 15:30:42 +1000

    Initial Commit

---
+++ selflogue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  selflogue
+//
+//  Created by Chew Jun Pin on 26/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
