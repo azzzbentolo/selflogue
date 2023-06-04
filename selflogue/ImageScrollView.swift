a56988e17c70ebd1c148c68d11671bea9abff943
Chew Jun PIn <jche0371@student.monash.edu>
2023-06-04 17:07:45 +1000

    Add unique date function

--- selflogue/ImageScrollView.swift
+++ selflogue/ImageScrollView.swift
@@ -8,68 +8,112 @@ struct ImagesScrollView: View {
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack {
-                ForEach(Array(imagesManager.imageFiles).sorted(by: { $0.value.2 > $1.value.2 }), id: \.key) { imageName, imageTuple in
-                    ZStack(alignment: .topTrailing) {
-                        VStack(alignment: .leading) {
-                            Image(uiImage: imageTuple.0)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 350, height: 350)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .padding(EdgeInsets(top: 50, leading: 30, bottom: 0, trailing: 30))
-                            
-                            Text(imageTuple.1)
-                                .font(.system(size: 15))
-                                .padding(.leading, 30)
-                                .padding(.trailing, 30)
-                                .padding(.top, 0)
-                                .padding(.bottom, 0)
-                            
-                            Text(format(date: imageTuple.2))
-                                .font(.system(size: 15))
-                                .padding(.leading, 30)
-                                .padding(.trailing, 30)
-                                .padding(.top, 0)
-                                .padding(.bottom, 10)
+                ForEach(getUniqueDates(), id: \.self) { date in
+                    VStack {
+                        HStack {
+                            Text(format(date: date))
+                                .font(.system(size: 20, weight: .bold)) // Increase font size
+                            Spacer()
                         }
-                        .padding(.bottom)
+                        .padding(.leading, 20)
+                        .padding(.top, 40)
+                        .padding(.bottom, 40)
                         
-                        Menu {
-                            Button(action: {
-                                selectedImage = imageName
-                                showingDeleteAlert = true
-                            }) {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            Button(action: {
-                                // Action for share button
-                            }) {
-                                Label("Share", systemImage: "square.and.arrow.up")
-                            }
-                        } label: {
-                            Image(systemName: "ellipsis")
-                                .font(.title)
-                                .foregroundColor(.black)
-                                .padding(.trailing, 30)
-                                .padding(.top, 40)
-                        }
-                        .padding(.top, -10)
-                    }
-                    .alert(isPresented: $showingDeleteAlert) {
-                        Alert(
-                            title: Text("Delete"),
-                            message: Text("Are you sure you want to delete this image?"),
-                            primaryButton: .destructive(Text("Delete")) {
-                                if let imageName = selectedImage {
-                                    imagesManager.deleteImage(named: imageName)
+                        ForEach(getImagesForDate(date), id: \.0) { imageName, imageTuple in
+                            ZStack(alignment: .topTrailing) {
+                                VStack(alignment: .leading) {
+                                    Image(uiImage: imageTuple.0)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 315, height: 315)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                        .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+                                    
+                                    Text(imageTuple.1)
+                                        .font(.system(size: 15))
+                                        .padding(.leading, 30)
+                                        .padding(.trailing, 30)
+                                        .padding(.top, 0)
+                                        .padding(.bottom, 0)
+                                    
+                                    Text(formatTime(date: imageTuple.2))
+                                        .font(.system(size: 15))
+                                        .padding(.leading, 30)
+                                        .padding(.trailing, 30)
+                                        .padding(.top, 0)
+                                        .padding(.bottom, 10)
                                 }
-                            },
-                            secondaryButton: .cancel()
-                        )
+                                .padding(.bottom)
+                                
+                                HStack {
+                                    Spacer()
+                                    
+                                    Menu {
+                                        Button(action: {
+                                            selectedImage = imageName
+                                            showingDeleteAlert = true
+                                        }) {
+                                            Label("Delete", systemImage: "trash")
+                                        }
+                                        Button(action: {
+                                            // Action for share button
+                                        }) {
+                                            Label("Share", systemImage: "square.and.arrow.up")
+                                        }
+                                    } label: {
+                                        Image(systemName: "ellipsis")
+                                            .font(.title)
+                                            .foregroundColor(.black)
+                                            .padding(.trailing, 30)
+                                            .padding(.top, -10)
+                                    }
+                                    .padding(.top, -10)
+                                }
+                            }
+                            .alert(isPresented: $showingDeleteAlert) {
+                                Alert(
+                                    title: Text("Delete"),
+                                    message: Text("Are you sure you want to delete this image?"),
+                                    primaryButton: .destructive(Text("Delete")) {
+                                        if let imageName = selectedImage {
+                                            imagesManager.deleteImage(named: imageName)
+                                        }
+                                    },
+                                    secondaryButton: .cancel()
+                                )
+                            }
+                        }
                     }
                 }
             }
         }
     }
     
+    func getUniqueDates() -> [Date] {
+        return Array(Set(imagesManager.imageFiles.values.map { $0.2.dateOnly() })).sorted(by: >)
+    }
+    
+    func getImagesForDate(_ date: Date) -> [(String, (UIImage, String, Date))] {
+        return imagesManager.imageFiles.filter { $0.value.2.dateOnly() == date }
+    }
+    
+    func format(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy"
+        return formatter.string(from: date)
+    }
+    
+    func formatTime(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: date)
+    }
+}
+
+extension Date {
+    func dateOnly() -> Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: self)
+        return calendar.date(from: components) ?? self
+    }
 }
